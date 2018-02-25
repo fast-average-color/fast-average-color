909ddf8065164e70ec1642865fa87a9e68bb239c
Denis Seleznev <hcodes@yandex.ru>
2018-02-25 11:46:01 +0300

    Add tests and update deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@
 
 const
     gulp = require('gulp'),
-    $ = require('gulp-load-plugins')(),
+    rename = require('gulp-rename'),
+    replace = require('gulp-replace'),
+    uglify = require('gulp-uglify'),
+    rollup = require('gulp-rollup'),
     babel = require('rollup-plugin-babel'),
     uglifyOptions = {output: {comments: /^!/}},
     copyright = '/*! Fast Average Color | © 2018 Denis Seleznev | MIT License | https://github.com/hcodes/fast-average-color/ */\n';
@@ -10,7 +13,7 @@ const
 gulp
     .task('js', function() {
         return gulp.src('src/*.js')
-            .pipe($.rollup({
+            .pipe(rollup({
                 input: 'src/index.js',
                 output: {
                     format: 'umd',
@@ -18,20 +21,20 @@ gulp
                 },
                 plugins: [babel()]
             }))
-            .pipe($.replace(/^/, copyright))
-            .pipe($.rename('index.js'))
+            .pipe(replace(/^/, copyright))
+            .pipe(rename('index.js'))
             .pipe(gulp.dest('dist/'));
     })
     .task('js.min', ['js'], function() {
         return gulp.src('dist/index.js')
-            .pipe($.rename('index.min.js'))
-            .pipe($.uglify(uglifyOptions))
+            .pipe(rename('index.min.js'))
+            .pipe(uglify(uglifyOptions))
             .pipe(gulp.dest('dist/'));
     })
     .task('es6', function() {
         return gulp.src('src/index.js')
-            .pipe($.rename('index.es6.js'))
-            .pipe($.replace(/^/, copyright))
+            .pipe(rename('index.es6.js'))
+            .pipe(replace(/^/, copyright))
             .pipe(gulp.dest('dist/'));
     })
     .task('watch', function() {
