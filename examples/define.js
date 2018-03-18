7c7dbc4d9e00e33474b7e0d3d87d2ddf3b957f04
Denis Seleznev <hcodes@yandex.ru>
2018-03-18 18:48:39 +0300

    Fix example

--- examples/define.js
+++ examples/define.js
@@ -2,8 +2,10 @@ var App = {
     init: function() {
         var
             that = this,
-            fac = new FastAverageColor({mode: 'precision'}),
-            input = document.querySelector('.select-file');
+            input = document.querySelector('.select-file'),
+            captureButton = document.querySelector('.capture-photo');
+
+        this._fac = new FastAverageColor({mode: 'precision'});
 
         input.onchange = function() {
             var
@@ -14,7 +16,7 @@ var App = {
                 var img = new Image();
                 img.src = reader.result;
 
-                fac.getColorFromUnloadedImage(img, function(color) {
+                that._fac.getColorFromUnloadedImage(img, function(color) {
                     that.addImage(img, file.name, color);
                 });
             };
@@ -22,6 +24,10 @@ var App = {
             if (file) {
                 reader.readAsDataURL(file);
             }
+        };
+
+        captureButton.onclick = function() {
+            that.capture();
         };
     },
     addImage: function(resource, name, color) {
@@ -47,6 +53,55 @@ var App = {
 
         resource.className = 'images__img';
         item.appendChild(resource);
+    },
+    capture: function() {
+        var that = this;
+
+        navigator.getUserMedia = navigator.getUserMedia ||
+                                 navigator.webkitGetUserMedia ||
+                                 navigator.mozGetUserMedia;
+
+        navigator.getUserMedia({ video: true }, function(mediaStream) {
+            // Firefox
+            if (!('readyState' in mediaStream)) {
+                mediaStream.readyState = 'live';
+            }
+
+            var
+                video = document.createElement('video'),
+                previewStream = new MediaStream(mediaStream);
+
+            if (HTMLMediaElement) {
+                video.srcObject = previewStream;  // Safari 11 doesn't allow use of createObjectURL for MediaStream
+            } else {
+                video.src = URL.createObjectURL(previewStream);
+            }
+
+            video.muted = true;
+            // Required by Safari on iOS 11. See https://webkit.org/blog/6784
+            video.setAttribute('playsinline', '');
+            video.play();
+            video.addEventListener('playing', function() {
+                setTimeout(function() {
+                    var canvas = document.createElement('canvas');
+                    canvas.width = video.videoWidth;
+                    canvas.height = video.videoHeight;
+
+                    var ctx = canvas.getContext('2d');
+                    ctx.drawImage(video, 0, 0);
+
+                    var image = new Image();
+                    image.src = canvas.toDataURL('image/png');
+
+                    that._fac.getColorFromUnloadedImage(image, function(color) {
+                        that.addImage(image, 'photo', color);
+                        mediaStream.stop();
+                    });
+                }, 500);
+            });
+        }, function() {
+            // console.log('failure to get media');
+        });
     }
 };
 
