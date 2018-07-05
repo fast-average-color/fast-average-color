ffb29d02fc1a76e12d847dc1ba7a5102004479e6
Denis Seleznev <hcodes@yandex.ru>
2018-07-05 08:52:57 +0300

    Update deps and Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,34 +10,37 @@ const
     uglifyOptions = {output: {comments: /^!/}},
     copyright = '/*! Fast Average Color | © 2018 Denis Seleznev | MIT License | https://github.com/hcodes/fast-average-color/ */\n';
 
-gulp
-    .task('js', function() {
-        return gulp.src('src/*.js')
-            .pipe(rollup({
-                input: 'src/index.js',
-                output: {
-                    format: 'umd',
-                    name: 'FastAverageColor'
-                },
-                plugins: [babel()]
-            }))
-            .pipe(replace(/^/, copyright))
-            .pipe(rename('index.js'))
-            .pipe(gulp.dest('dist/'));
-    })
-    .task('js.min', ['js'], function() {
-        return gulp.src('dist/index.js')
-            .pipe(rename('index.min.js'))
-            .pipe(uglify(uglifyOptions))
-            .pipe(gulp.dest('dist/'));
-    })
-    .task('es6', function() {
-        return gulp.src('src/index.js')
-            .pipe(rename('index.es6.js'))
-            .pipe(replace(/^/, copyright))
-            .pipe(gulp.dest('dist/'));
-    })
-    .task('watch', function() {
-        gulp.watch(['src/**/*', 'examples/**/*']);
-    })
-    .task('default', ['js.min', 'es6']);
+gulp.task('js', function() {
+    return gulp.src('src/*.js')
+        .pipe(rollup({
+            input: 'src/index.js',
+            output: {
+                format: 'umd',
+                name: 'FastAverageColor'
+            },
+            plugins: [babel()]
+        }))
+        .pipe(replace(/^/, copyright))
+        .pipe(rename('index.js'))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('js.min', gulp.series('js', function() {
+    return gulp.src('dist/index.js')
+        .pipe(rename('index.min.js'))
+        .pipe(uglify(uglifyOptions))
+        .pipe(gulp.dest('dist/'));
+}));
+
+gulp.task('es6', function() {
+    return gulp.src('src/index.js')
+        .pipe(rename('index.es6.js'))
+        .pipe(replace(/^/, copyright))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['src/**/*', 'examples/**/*']);
+});
+
+gulp.task('default', gulp.series('js.min', 'es6'));
