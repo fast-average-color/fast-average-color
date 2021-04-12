7e2f0ba92ab7947958624686776ded6bd1dfc4ed
Denis Seleznev <hcodes@yandex.ru>
2021-04-13 00:13:23 +0300

    :cyclone:

--- dist/index.es6.js
+++ dist/index.es6.js
@@ -1,4 +1,4 @@
-/*! Fast Average Color | © 2020 Denis Seleznev | MIT License | https://github.com/fast-average-color/fast-average-color */
+/*! Fast Average Color | © 2021 Denis Seleznev | MIT License | https://github.com/fast-average-color/fast-average-color */
 function toHex(num) {
     const str = num.toString(16);
 
@@ -130,7 +130,8 @@ function dominantAlgorithm(arr, len, options) {
     const divider = 24;
     const ignoredColor = options.ignoredColor;
     const step = options.step;
-
+    let max = [0, 0, 0, 0, 0];
+    
     for (let i = 0; i < len; i += step) {
         const red = arr[i];
         const green = arr[i + 1];
@@ -156,18 +157,11 @@ function dominantAlgorithm(arr, len, options) {
         } else {
             colorHash[key] = [red * alpha, green * alpha, blue * alpha, alpha, 1];
         }
+        
+        if (max[4] < colorHash[key][4]) {
+            max = colorHash[key];
+        }
     }
-
-    const buffer = Object.keys(colorHash)
-        .map(key => colorHash[key])
-        .sort((a, b) => {
-            const countA = a[4];
-            const countB = b[4];
-
-            return countA > countB ?  -1 : countA === countB ? 0 : 1;
-        });
-
-    const max = buffer[0];
 
     const redTotal = max[0];
     const greenTotal = max[1];
