852e98172b168845dfb9e303ae19dd72518bb9b1
Denis Seleznev <hcodes@users.noreply.github.com>
2022-06-20 16:57:11 +0300

    :cyclone:

--- dist/index.common.js
+++ dist/index.common.js
@@ -1,6 +1,8 @@
 /*! Fast Average Color | © 2022 Denis Seleznev | MIT License | https://github.com/fast-average-color/fast-average-color */
 'use strict';
 
+Object.defineProperty(exports, '__esModule', { value: true });
+
 function toHex(num) {
     var str = num.toString(16);
     return str.length === 1 ? '0' + str : str;
@@ -318,13 +320,12 @@ var FastAverageColor = /** @class */ (function () {
      * Get asynchronously the average color from not loaded image.
      */
     FastAverageColor.prototype.getColorAsync = function (resource, options) {
-        var _a;
         if (!resource) {
             return Promise.reject(getError('call .getColorAsync() without resource.'));
         }
         if (typeof resource === 'string') {
             var img = new Image();
-            img.crossOrigin = (_a = options === null || options === void 0 ? void 0 : options.crossOrigin) !== null && _a !== void 0 ? _a : '';
+            img.crossOrigin = options && options.crossOrigin || '';
             img.src = resource;
             return this.bindImageEvents(img, options);
         }
@@ -467,4 +468,4 @@ var FastAverageColor = /** @class */ (function () {
     return FastAverageColor;
 }());
 
-module.exports = FastAverageColor;
+exports.FastAverageColor = FastAverageColor;
