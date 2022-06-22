442cc57f309e742dbcad2ca0426a1c68d51ac010
Denis Seleznev <hcodes@users.noreply.github.com>
2022-06-22 14:10:13 +0300

    Remove dist/

--- dist/algorithm/simple.d.ts
+++
@@ -1,2 +0,0 @@
-import { FastAverageColorAlgorithmOptions, FastAverageColorRgba } from '../index';
-export declare function simpleAlgorithm(arr: number[] | Uint8ClampedArray | Uint8Array, len: number, options: FastAverageColorAlgorithmOptions): FastAverageColorRgba;
--- dist/helpers/error.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const ERROR_PREFIX = "FastAverageColor: ";
-export declare function outputError(message: string, silent?: boolean, error?: unknown): void;
-export declare function getError(text: string): Error;
